55996c1994caf96934ac410d8daf85982135b29b
Azran Hossain <mr69751@gmail.com>
2021-07-27 19:55:45 +0600

    comments added

--- BankDatabaseAccess/DatabaseOperation/IOperations.cs
+++ BankDatabaseAccess/DatabaseOperation/IOperations.cs
@@ -8,9 +8,23 @@ namespace BankDatabaseAccess.DatabaseOperation
 {
     interface IOperations
     {
-
+        /// <summary>
+        /// Insert Data to the database
+        /// </summary>
+        /// <param name="personModel">Customer or Employee Model</param>
+        /// <returns></returns>
         int Insert(EntityModel.PersonModel personModel);
+        /// <summary>
+        /// Update data from databse
+        /// </summary>
+        /// <param name="personModel">Customer or Employee Model</param>
+        /// <returns></returns>
         int Update(EntityModel.PersonModel personModel);
+        /// <summary>
+        /// Delete data from database
+        /// </summary>
+        /// <param name="personModel">Customer or Employee Model</param>
+        /// <returns></returns>
         int Delete(EntityModel.PersonModel personModel);
 
     }
