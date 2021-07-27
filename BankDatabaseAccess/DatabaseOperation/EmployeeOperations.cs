0af83b10832c9d25f5ccdcaf4995acb722d2c345
Azran Hossain <mr69751@gmail.com>
2021-07-27 18:51:54 +0600

    DB Operation added

---
+++ BankDatabaseAccess/DatabaseOperation/EmployeeOperations.cs
@@ -0,0 +1,28 @@
+﻿using BankDatabaseAccess.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDatabaseAccess.DatabaseOperation
+{
+    public class EmployeeOperations : IOperations
+    {
+        //TODO - make them work
+        public int Delete(PersonModel personModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(PersonModel personModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(PersonModel personModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BankDatabaseAccess/DatabaseOperation/IOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDatabaseAccess.DatabaseOperation
+{
+    interface IOperations
+    {
+
+        int Insert(EntityModel.PersonModel personModel);
+        int Update(EntityModel.PersonModel personModel);
+        int Delete(EntityModel.PersonModel personModel);
+
+    }
+}
