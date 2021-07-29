0cec94735b49bd8a6e1f15e4f310b7b628c227e7
Azran Hossain <mr69751@gmail.com>
2021-07-29 19:39:52 +0600

    GridStyle added / code optimize

--- BankManagementSystem/EmployeeDashboardForms/CustomerInfo.cs
+++ BankManagementSystem/EmployeeDashboardForms/CustomerInfo.cs
@@ -8,9 +8,13 @@ namespace BankManagementSystem.EmployeeDashboardForms
         public CustomerInfo()
         {
             InitializeComponent();
+            GridStyle();
+            CustomerdataGridView.DataSource = new DataReader().GetAllData(customer: true);
+        }
+        private void GridStyle()
+        {
             CustomerdataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
             CustomerdataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
-            CustomerdataGridView.DataSource = new DataReader().GetAllData(customer: true);
         }
     }
 }
