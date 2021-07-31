894ced302bfa53b9ae60b2ee7e8fc142b54edacb
Azran Hossain <mr69751@gmail.com>
2021-07-31 21:05:45 +0600

    [Issue] Todo added

--- BankManagementSystem/EmployeeDashboardForms/CustomerInfo.cs
+++ BankManagementSystem/EmployeeDashboardForms/CustomerInfo.cs
@@ -13,6 +13,8 @@ namespace BankManagementSystem.EmployeeDashboardForms
         }
         private void GridStyle()
         {
+            // TODO - Fix Balance precision. Only two digit after balance
+            // TODO - Fix Account Create Date. Only date will be enough.Time is not needed 
             CustomerdataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
             CustomerdataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
         }
